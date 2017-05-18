bed715d277e7a2ac1b62e3ad17541696110bf90c
Agathe Cocco <Agathe.Cocco@worldstores-grp.co.uk>
2017-05-18 15:08:00 +0100

    improved UI, added date

--- app.js
+++ app.js
@@ -1,126 +1,136 @@
-var listController = (function () {
-	//var todos = [];
-	counter = 0;
-	return {
-		addItem: function () {
-			//todos.push(item);
-			counter +=1;
-		},
-
-		deleteItem: function (item) {
-			counter -=1;
-			//var i = todos.indexOf(item);
-			//if (i != -1) {
-				//todos.splice(i, 1);
-
-			//}
-		},
-	
-		returnTotalItems: function () {
-			return counter;
-		},
-		
-		testing: function () {
-			//console.log(todos);
-			console.log(counter);
-		}
-	}
+var listController = (function() {
+    //var todos = [];
+    counter = 0;
+    return {
+        addItem: function() {
+            //todos.push(item);
+            counter += 1;
+        },
+        deleteItem: function(item) {
+            counter -= 1;
+            //var i = todos.indexOf(item);
+            //if (i != -1) {
+            //todos.splice(i, 1);
+            //}
+        },
+        returnTotalItems: function() {
+            return counter;
+        },
+        testing: function() {
+            //console.log(todos);
+            console.log(counter);
+        }
+    }
 })();
+var UIController = (function() {
+    var DOMstrings = {
+        inputDescription: '.add__description',
+        inputBtn: '.add__btn',
+        todosContainer: '.todos__list',
+        container: '.container',
+        totalItems: '.total__items',
+        addField: '.add'
+    };
+    var items = listController.returnTotalItems();
+    return {
+        getInput: function() {
+            return document.querySelector(DOMstrings.inputDescription).value;
+        },
+        addListItem: function() {
+            var items = listController.returnTotalItems();
+            var html, newHtml, element;
+            element = DOMstrings.todosContainer;
+            var colors = ['#FFBE0B', '#FB5607', '#FF006E', '#8338EC', '#3A86FF'];
+            var gradients = [
+            'linear-gradient(to bottom, rgba(255,190,11, 0.9) 0%, rgba(255,190,11, 0.5) 100%)', 
+            'linear-gradient(to bottom, rgba(251, 86, 7, 0.9) 0%, rgba(251, 86, 7, 0.5) 100%)',
+            'linear-gradient(to bottom, rgba(255,0,110, 0.9) 0%, rgba(255,0,110, 0.5) 100%)',
+            'linear-gradient(to bottom, rgba(131, 56, 236, 0.9) 0%, rgba(131, 56, 236, 0.5) 100%)',
+            'linear-gradient(to bottom, rgba(58,134,255, 0.9) 0%, rgba(58,134,255, 0.5) 100%)'
+             ]
+            var random_gradient = gradients[Math.floor(Math.random() * gradients.length)];
+            html = '<div class="item" style="%style%"> <div class="item__description">%description%</div><div class="right"><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            newHtml = html.replace('%description%', document.querySelector(DOMstrings.inputDescription).value);
+            newHtml = newHtml.replace('%style%', 'background:' + random_gradient + ', url(back.png)') ;
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+        clearField: function() {
+            document.querySelector(DOMstrings.inputDescription).value = '';
+            document.querySelector(DOMstrings.inputDescription).focus();
+        },
+        displayTotalItems: function() {
+            var items = listController.returnTotalItems();
+            if (items == 0) {
+                document.querySelector(DOMstrings.totalItems).textContent = 'nothing';
+            } else if (items == 1) {
+                document.querySelector(DOMstrings.totalItems).textContent = ' 1 thing';
+            } else {
+                document.querySelector(DOMstrings.totalItems).textContent = items + ' things';
+            }
+        },
+        displayDate: function () {
+        	var mydate = new Date();
+    		var year = mydate.getYear();
+    		if (year < 1000) {
+        		year+=1900;
+    		}
+            var day = mydate.getDay();
+            var month = mydate.getMonth();
+            var daym = mydate.getDate();
+            if (daym < 10) {
+            	daym = "0" + daym;
+            }
+            var dayarray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            var montharray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            var today = dayarray[day] + " "+ daym + " "+ montharray[month] + " " + year;
+            document.getElementById('date').innerHTML = today;
+        },
 
-var UIController = (function () {
-	var DOMstrings = {
-		inputDescription: '.add__description',
-		inputBtn: '.add__btn',
-		todosContainer: '.todos__list',
-		container: '.container',
-		totalItems: '.total__items'
-	};
-	var items = listController.returnTotalItems();
-	return {
-		getInput: function () {
-			return document.querySelector(DOMstrings.inputDescription).value;
-		},
-		
-		addListItem: function() {
-			var items = listController.returnTotalItems();
-			var html, newHtml, element;
-			element = DOMstrings.todosContainer;
-			var colors = ['#FFBE0B', '#FB5607', '#FF006E', '#8338EC', '#3A86FF'];
-			var random_color = colors[Math.floor(Math.random() * colors.length)];
-			html = '<div class="item" id="%id%" style="%style%"> <div class="item__description">%description%</div><div class="right"><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
-			newHtml = html.replace('%description%', document.querySelector(DOMstrings.inputDescription).value);
-			newHtml = newHtml.replace('%id%', 'item-' + (items));
-			newHtml = newHtml.replace('%style%', 'background-color:' + random_color);
-			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);	
-		},
-		
-		clearField: function () {
-			document.querySelector(DOMstrings.inputDescription).value = '';
-			document.querySelector(DOMstrings.inputDescription).focus();
-		},
-
-		displayTotalItems: function () {
-		var items = listController.returnTotalItems();
-			if (items == 0) {
-				document.querySelector(DOMstrings.totalItems).textContent = 'nothing';
-			}
-			else if (items == 1){
-				document.querySelector(DOMstrings.totalItems).textContent = ' 1 thing';
-			}
-			else {
-				document.querySelector(DOMstrings.totalItems).textContent = items + ' things';
-			}
-		},
-
-		getDOMstrings: function () {
-			return DOMstrings;
-		},
-		
-	};
-
+        getDOMstrings: function() {
+            return DOMstrings;
+        },
+    };
 })();
-
-var controller = (function (listCtrl, UICtrl) {
-	var DOM = UICtrl.getDOMstrings();
-	var setupEventListeners = function () {
-		document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-		document.addEventListener('keypress', function (e) {
-			if (e.keyCode === 13 || e.which === 13) {
-				ctrlAddItem();
-			}
-		});
-		document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
-	};
-
-	var updateTotalItems = function () {
-		listController.returnTotalItems();
-		UICtrl.displayBudget(budget);
-	};
-
-	var ctrlAddItem = function () {
-		if (document.querySelector(DOM.inputDescription).value !== '') {
-			listCtrl.addItem();
-			UICtrl.addListItem();
-			UICtrl.clearField();
-			UICtrl.displayTotalItems();
-		}
-	};
-
-	var ctrlDeleteItem = function (e) {
-		listCtrl.deleteItem();
-		var todo = e.target.parentNode.parentNode.parentNode.parentNode;
-			if (e.target && e.target.className === "ion-ios-close-outline") {
-				todo.parentNode.removeChild(todo);
-			}
-		UICtrl.displayTotalItems();
-	};
-
-	return {
-		init: function () {
-			UICtrl.displayTotalItems();
-			setupEventListeners();
-		}
-	};
+var controller = (function(listCtrl, UICtrl) {
+    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+        document.querySelector(DOM.inputBtn).addEventListener('click', showAddField);
+        document.addEventListener('keypress', function(e) {
+            if (e.keyCode === 13 || e.which === 13) {
+                ctrlAddItem();
+            }
+        });
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+    };
+    var updateTotalItems = function() {
+        listController.returnTotalItems();
+        UICtrl.displayBudget(budget);
+    };
+    var ctrlAddItem = function() {
+        if (document.querySelector(DOM.inputDescription).value !== '') {
+            listCtrl.addItem();
+            UICtrl.addListItem();
+            UICtrl.clearField();
+            UICtrl.displayTotalItems();
+        }
+    };
+    var showAddField = function() {
+        document.querySelector(DOM.addField).style.display = 'block';
+    }
+    var ctrlDeleteItem = function(e) {
+        var todo = e.target.parentNode.parentNode.parentNode.parentNode;
+        if (e.target && e.target.className === "ion-ios-close-outline") {
+            todo.parentNode.removeChild(todo);
+            listCtrl.deleteItem();
+        }
+        UICtrl.displayTotalItems();
+    };
+    return {
+        init: function() {
+            UICtrl.displayTotalItems();
+            UICtrl.displayDate();
+            setupEventListeners();
+        }
+    };
 })(listController, UIController);
-
 controller.init();
